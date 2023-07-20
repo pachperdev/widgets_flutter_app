6f579aeb777d95467f644d1f3c41da438025e445
Mauricio Pacheco <pachperdev@gmail.com>
2023-07-20 11:50:44 -0500

    ListView

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -2,29 +2,26 @@ import 'package:flutter/material.dart';
 
 class MenuItem {
   final String title;
-  final String subtitle;
+  final String subTitle;
   final String link;
   final IconData icon;
 
-  const MenuItem({
-    required this.title,
-    required this.subtitle,
-    required this.link,
-    required this.icon,
-  });
-
-  static const appMenuItems = <MenuItem>[
-    MenuItem(
-      title: 'Botones',
-      subtitle: 'Varios botones en Flutter',
-      link: '/buttons',
-      icon: Icons.smart_button_outlined,
-    ),
-    MenuItem(
-      title: 'Tarjetas',
-      subtitle: 'Un contenedor estilizado',
-      link: '/about',
-      icon: Icons.credit_card,
-    ),
-  ];
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
 }
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      title: 'Botones',
+      subTitle: 'Varios botones en Flutter',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
+  MenuItem(
+      title: 'Tarjetas',
+      subTitle: 'Un contenedor estilizado',
+      link: '/cards',
+      icon: Icons.credit_card),
+];
