57c8f645f50faec83a340748d470e012f844995d
Mauricio Pacheco <pachperdev@gmail.com>
2023-07-23 12:33:37 -0500

    Different pre-configured buttons

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons';
@@ -7,8 +8,60 @@ class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ButtonsScreen'),
+        appBar: AppBar(
+          title: const Text('ButtonsScreen'),
+        ),
+        body: const _ButtonsView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: const Icon(Icons.arrow_back_ios_new_rounded),
+        ));
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {}, child: const Text('ElevatedButton')),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text('Elevate icon'),
+            ),
+            const ElevatedButton(
+                onPressed: null, child: Text('ElevatedButton disabled')),
+            FilledButton(onPressed: () {}, child: const Text('FilledButton')),
+            FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text('FilledButton icon')),
+            OutlinedButton(
+                onPressed: () {}, child: const Text('OutlinedButton')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text('OutlinedButton icon')),
+            TextButton(onPressed: () {}, child: const Text('TextButton')),
+            TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text('TextButton icon')),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+          ],
+        ),
       ),
     );
   }
