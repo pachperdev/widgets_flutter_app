29f97c701c7d3fb3091bf4df9b51eba7dec96f8c
Mauricio Pacheco <pachperdev@gmail.com>
2023-08-20 19:10:02 -0500

    Linear and Circular progress controlled

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,3 @@
-// GoRouter configuration
 import 'package:go_router/go_router.dart';
 
 import '../../presentation/screens/screens.dart';
@@ -8,15 +7,23 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
     GoRoute(
       path: '/cards',
+      name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
+    ),
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen(),
     ),
   ],
 );
