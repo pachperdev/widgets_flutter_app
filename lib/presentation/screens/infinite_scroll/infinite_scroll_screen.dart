e13efe6c8961c9d45cb2cc8911dc7271dbf00ebf
Mauricio Pacheco <pachperdev@gmail.com>
2023-08-25 23:22:24 -0500

    refreshIndicator

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -55,6 +55,20 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     //TODO: mover scroll
   }
 
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    if (!isMounted) return;
+
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,18 +76,24 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       body: MediaQuery.removePadding(
         context: context,
         removeTop: true,
-        child: ListView.builder(
-          controller: scrollController,
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 10,
+          strokeWidth: 2,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
                 fit: BoxFit.cover,
                 width: double.infinity,
                 height: 300,
                 placeholder: const AssetImage('assets/images/loading.gif'),
                 image: NetworkImage(
-                    'https://picsum.photos/id/${imagesIds[index]}/500/300/'));
-          },
+                    'https://picsum.photos/id/${imagesIds[index]}/500/300/'),
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
