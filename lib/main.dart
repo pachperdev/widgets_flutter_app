31f92e323d57734a45924e86928e4c37fcc24fcd
Mauricio Pacheco <pachperdev@gmail.com>
2024-01-27 19:00:38 -0500

    Add selected color provider and update app theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,30 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_flutter_app/config/router/app_router.dart';
 import 'package:widgets_flutter_app/config/theme/app_theme.dart';
 
+import 'presentation/provider/theme_provider.dart';
+
 void main() {
   runApp(
-    ProviderScope(
-      child: const MainApp(),
+    const ProviderScope(
+      child: MainApp(),
     ),
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+    final int selectedColor = ref.watch(selectedColorProvider);
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 11).getTheme(),
+      theme: AppTheme(
+        selectedColor: selectedColor,
+        isDarkMode: isDarkMode,
+      ).getTheme(),
     );
   }
 }
